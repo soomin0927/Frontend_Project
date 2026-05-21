ebb21ca2b18a5b0b3ea6fe7ba5c42c3bd0c85726
soomin0927 <ehfvls0927@naver.com>
2026-05-22 01:52:36 +0900

    feat: 시간 계산 함수 만들기

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,1 +1,26 @@
-// 시간 계산 전용
+// 시간 계산
+
+// 입력받은 시간을 통해 px 로 변경
+export const timeToMinutes = ( 
+    time: string
+) => {
+    const [hour, minute] = time.split(':').map(Number);
+    return hour * 60 + minute; // ex) 9,0 => 9*60+0 = 540
+}
+
+
+// 블록 높이 계산 
+const HOUR_HEIGHT = 80; // 80px -> 지정해놓은 1시간 한칸의 높이 
+
+export const calculateBlockHeight = (
+    startTime: string,
+    endTime: string
+) => {
+    const start = timeToMinutes(startTime);
+    const end = timeToMinutes(endTime);
+
+    return(
+        (end - start) / 60
+    ) * HOUR_HEIGHT;
+};
+
